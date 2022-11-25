a7fc100f99f2bba35572d80b2b1d805fb21a4c49
Dgeko <dfhdfh13@gmail.com>
2022-11-25 21:57:38 +0300

    создал и настроил роутинг

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import Post from './models/post.js'
+import router from './router/router.js';
 
 const PORT = 3000;
 
@@ -8,17 +8,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/api', router);
+
 const db_url = 'mongodb+srv://Dgeko:frGGT14@cluster0.5rbuesr.mongodb.net/node-second?retryWrites=true&w=majority'
 
-app.post('/', async (req, res) => {
-    try { 
-    const {author, title, content, picture} = req.body
-    const post = await Post.create({author, title, content, picture})
-    res.json(post);
-    } catch (error){
-        res.status(500).json(error)
-    }
-});
+
 
 async function startApp() {
     try {
