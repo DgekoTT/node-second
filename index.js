ab2f76606e2c82662e4c1661ac595d209db53ed1
Dgeko <dfhdfh13@gmail.com>
2022-11-25 17:21:43 +0300

    начало второго проекта на Node.js подключаю бд mongodb

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+
+const db_url = 'mongodb+srv://Dgeko:frGGT1@#4@cluster0.5rbuesr.mongodb.net/?retryWrites=true&w=majority'
 
 app.post('/', (req, res) => {
     console.log(req.body);
